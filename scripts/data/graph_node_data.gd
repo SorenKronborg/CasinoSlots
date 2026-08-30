6b1863d7810bd800ca7eeb0e01970b5f90756efc
soren kronborg <sf@halfspace.io>
2026-08-30 19:08:42 +0200

    delete

--- scripts/data/graph_node_data.gd
+++
@@ -1,8 +0,0 @@
-class_name GraphNodeData
-extends Resource
-
-@export var id: StringName
-@export var position: Vector2
-@export var label: String = ""
-@export var is_start: bool = false
-@export var coin_cost: int = 10
--- scripts/data/level_definition.gd
+++
@@ -1,5 +0,0 @@
-class_name LevelDefinition
-extends Resource
-
-@export var nodes: Array[GraphNodeData] = []
-@export var edges: Array[GraphEdgeData] = []
