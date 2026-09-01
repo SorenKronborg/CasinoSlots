531de7d1fe4a11d5e21ee4c420a393e0487d6588
soren kronborg <sf@halfspace.io>
2026-09-01 14:14:48 +0200

    added save and load functionallity

--- features/game_idle/idle_game_scene.gd
+++ features/game_idle/idle_game_scene.gd
@@ -4,13 +4,12 @@ extends Control
 @export var level_scene: PackedScene
 @export var mission_1_definition: LevelDefinition
 @export var mission_2_definition: LevelDefinition
-
-var resources: Dictionary = {
-	"prestige": 5,
-}
+@export_file("*.tscn") var main_menu_scene_path: String
 
 
 func _ready() -> void:
+	%SaveGame.text = tr("Save Game")
+	%QuitGame.text = tr("Quit Game")
 	%Upgrades.text = tr("Upgrades")
 	%Mission1.text = tr("Mission 1")
 	%Mission2.text = tr("Mission 2")
@@ -21,7 +20,17 @@ func _ready() -> void:
 
 func _refresh_resource_info() -> void:
 	%PrestigeLabel.text = tr("Prestige")
-	%PrestigeValue.text = str(resources["prestige"])
+	%PrestigeValue.text = str(GameState.resources["prestige"])
+
+
+func _on_save_game_pressed() -> void:
+	var err := GameState.save_game()
+	if err != OK:
+		push_error("Failed to save game: %s" % error_string(err))
+
+
+func _on_quit_game_pressed() -> void:
+	get_tree().change_scene_to_file(main_menu_scene_path)
 
 
 func _on_upgrades_pressed() -> void:
--- features/menu/main_menu.gd
+++ features/menu/main_menu.gd
@@ -6,12 +6,23 @@ extends Control
 
 func _ready() -> void:
 	%NewGame.text = tr("New Game")
-	%LoadGame.text = tr("Load Game")
+	%ContinueGame.text = tr("Continue Game")
+	%ContinueGame.disabled = not GameState.has_save()
 	%Options.text = tr("Options")
 	%Quit.text = tr("Quit")
 
 
 func _on_new_game_pressed() -> void:
+	GameState.reset()
+	get_tree().change_scene_to_packed(idle_game_scene)
+
+
+func _on_continue_game_pressed() -> void:
+	var err := GameState.load_game()
+	if err != OK:
+		push_error("Failed to continue game: %s" % error_string(err))
+		%ContinueGame.disabled = not GameState.has_save()
+		return
 	get_tree().change_scene_to_packed(idle_game_scene)
 
 
